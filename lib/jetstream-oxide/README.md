def8afce2736ce09bd8e388f3e1f825e2d235af7
Jack Platten <jack@platten.me>
2025-01-24 12:05:17 -0800

    upstream git reference

--- lib/jetstream-oxide/README.md
+++
@@ -1,1 +0,0 @@
-sourced from [here](https://github.com/videah/jetstream-oxide/issues/3#issuecomment-2611449764) for testing purposes
--- lib/jetstream-oxide/src/exports.rs
+++
@@ -1,8 +0,0 @@
-//! Useful exports for third-party crates used by this project.
-
-pub use atrium_api::types::string::{
-    Cid,
-    Did,
-    Handle,
-    Nsid,
-};
