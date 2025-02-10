4c6f8feb419b572f9e2a2c8a5ea4974420f479b6
Jack Platten <jack@platten.me>
2025-02-09 23:00:16 -0800

    first batch of clippy

--- src/post_listener.rs
+++ src/post_listener.rs
@@ -67,52 +67,46 @@ pub async fn websocket_listener(pool: sqlx::Pool<sqlx::Postgres>) {
     while let Ok(event) = receiver.recv_async().await {
         log::info!("[jetstream] received event");
         if let Commit(commit) = event {
-            match commit {
-                CommitEvent::Create { info, commit } => {
-                    if let AppBskyFeedPost(record) = commit.record {
-                        // check and see if this post is what we're looking for
-                        log::info!("[jetstream] Checking record: {}", record.text);
-                        if record.text.starts_with(target_emoji.as_str()) {
-                            let facets = record.facets.clone().unwrap();
-                            for facet in facets {
-                                for feature in &facet.features {
-                                    match feature {
-                                        atrium_api::types::Union::Refs(MainFeaturesItem::Link(
-                                            link,
-                                        )) => {
-                                            let rkey = commit.info.rkey.clone();
-                                            let uri = link.uri.clone();
-                                            // get the slug
-                                            let uri_parts: Vec<&str> = uri.split('/').collect();
-                                            let slug = *uri_parts.last().unwrap();
-                                            let time_us_string = info.time_us.to_string();
-                                            let time_us = time_us_string.as_str();
+            if let CommitEvent::Create { info, commit } = commit {
+                if let AppBskyFeedPost(record) = commit.record {
+                    // check and see if this post is what we're looking for
+                    log::info!("[jetstream] Checking record: {}", record.text);
+                    if record.text.starts_with(target_emoji.as_str()) {
+                        let facets = record.facets.clone().unwrap();
+                        for facet in facets {
+                            for feature in &facet.features {
+                                if let atrium_api::types::Union::Refs(MainFeaturesItem::Link(
+                                        link,
+                                    )) = feature {
+                                    let rkey = commit.info.rkey.clone();
+                                    let uri = link.uri.clone();
+                                    // get the slug
+                                    let uri_parts: Vec<&str> = uri.split('/').collect();
+                                    let slug = *uri_parts.last().unwrap();
+                                    let time_us_string = info.time_us.to_string();
+                                    let time_us = time_us_string.as_str();
 
-                                            match sqlx::query("INSERT INTO posts (slug, rkey, time_us) VALUES ($1, $2, $3) ON CONFLICT (slug) DO NOTHING")
-                                                .bind(slug)
-                                                .bind(rkey)
-                                                .bind(time_us)
-                                                .execute(& pool)
-                                                .await {
-                                                    Ok(_) => {
-                                                        log::info!("[jetstream] Inserted post");
-                                                    }
-                                                    Err(e) => {
-                                                        log::error!(
-                                                            "[jetstream] Failed to insert post: {}",
-                                                            e
-                                                        );
-                                                    }
-                                                }
+                                    match sqlx::query("INSERT INTO posts (slug, rkey, time_us) VALUES ($1, $2, $3) ON CONFLICT (slug) DO NOTHING")
+                                        .bind(slug)
+                                        .bind(rkey)
+                                        .bind(time_us)
+                                        .execute(& pool)
+                                        .await {
+                                            Ok(_) => {
+                                                log::info!("[jetstream] Inserted post");
+                                            }
+                                            Err(e) => {
+                                                log::error!(
+                                                    "[jetstream] Failed to insert post: {}",
+                                                    e
+                                                );
+                                            }
                                         }
-                                        _ => {} // ick
-                                    }
                                 }
                             }
                         }
                     }
                 }
-                _ => {}
             }
         }
     }
